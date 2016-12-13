4d3599a19910105f90bd6d10f55ad8d81fed2bd2
vagrant <vagrant@ubuntu-1404.vagrantup.com>
2016-12-13 16:56:21 +0000

    changes

--- labs/lab03/SimpleSample/src/main/java/example/SimpleSample.java
+++ labs/lab03/SimpleSample/src/main/java/example/SimpleSample.java
@@ -150,7 +150,7 @@ public class SimpleSample extends ChaincodeBase {
 
 	@Override
 	public String getChaincodeID() {
-		return "SimpleSample";
+			return "BancoLSachini";
 	}
 
 	public static void main(String[] args) throws Exception {
